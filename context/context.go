6f4c0f4403c981ba7e292590fab46a5789e3ae9b
Alexander Ewetumo <trinoxf@gmail.com>
2017-02-27 14:10:51 +0100

    changes: adding changes for context

--- context/context.go
+++ context/context.go
@@ -2,7 +2,10 @@
 // library and as an experiement in such a library works.
 package context
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 //==============================================================================
 
@@ -75,28 +78,29 @@ func (p *Pair) Get(key interface{}) (value interface{}, found bool) {
 // build passable context around.
 type Context interface {
 
-	//Cancel cancels out the timer setup to nil out contexts internal store.
-	Cancel()
-
 	// IsExpired returns true/false if the context is considered expired.
 	IsExpired() bool
 
 	// Get returns the giving value for the provided key if it exists else nil.
 	Get(key interface{}) (interface{}, bool)
 
-	// WhenExpired returns a channel which gets closed when the given channel
+	// Done returns a channel which gets closed when the given channel
 	// expires else closes immediately if its not an expiring context.
-	WhenExpired() <-chan struct{}
+	Done() <-chan struct{}
+
+	// Ctx returns a Context which exposes a basic context interface without  the
+	// cancellable method.
+	Ctx() Context
 
 	// New returns a new context based on the fileds of the context which its
 	// called from, it does inherits the lifetime limits of the context its
 	// called from.
-	New() Context
+	New(cancelWithParent bool) CancelableContext
 
 	// WithTimeout returns a new Context from the previous with the given timeout
 	// if the timeout is still further than the previous in expiration date else uses
 	// the previous expiration date instead since that is still further in the future.
-	WithTimeout(timeout time.Duration) Context
+	WithTimeout(timeout time.Duration, cancelWithParent bool) CancelableContext
 
 	// Set adds a key and value pair into the context store.
 	Set(key interface{}, value interface{})
@@ -106,7 +110,7 @@ type Context interface {
 
 	// WithValue returns a new context then adds the key and value pair into the
 	// context's store.
-	WithValue(key interface{}, value interface{}) Context
+	WithValue(key interface{}, value interface{}) CancelableContext
 
 	// TimeRemaining returns the remaining time for expiring of the context if it
 	// indeed has an expiration date set and returns a bool value indicating if it
@@ -114,10 +118,20 @@ type Context interface {
 	TimeRemaining() (remaining time.Duration, hasTimeout bool)
 }
 
+// CancelableContext defines a interface which exposes the capability to cancel
+// a Context.
+type CancelableContext interface {
+	Context
+
+	//Cancel cancels out the timer setup to nil out contexts internal store.
+	Cancel()
+}
+
 // New returns a new context instance.
-func New() Context {
+func New() CancelableContext {
 	cl := context{
-		fields: nilPair,
+		fields:    nilPair,
+		canceller: make(chan struct{}),
 	}
 	return &cl
 }
@@ -128,31 +142,33 @@ func New() Context {
 // use cases with a explicitly set duration which clears all its internal
 // data after the giving period.
 type context struct {
-	fields   *Pair
-	lifetime time.Time
-	timer    *time.Timer
-	duration time.Duration
-	parent   Context
+	fields    *Pair
+	lifetime  time.Time
+	timer     *time.Timer
+	duration  time.Duration
+	parent    Context
+	canceller chan struct{}
+	cl        sync.Mutex
+	canceled  bool
 }
 
 // New returns a new context from with the configuration limits of this one.
-func (c *context) New() Context {
+func (c *context) New(cancelWithParent bool) CancelableContext {
 	if c.timer != nil {
-		return c.WithTimeout(c.duration)
+		return c.WithTimeout(c.duration, cancelWithParent)
 	}
 
-	return c.newChild()
+	return c.newChild(cancelWithParent)
 }
 
 // WithTimeout returns a new context whoes internal value expires
 // after the giving duration.
-func (c *context) WithTimeout(life time.Duration) Context {
-	child := c.newChild()
-
-	lifetime := time.Now().Add(life)
+func (c *context) WithTimeout(life time.Duration, cancelWithParent bool) CancelableContext {
+	child := c.newChild(cancelWithParent)
 
 	var useChild bool
 
+	lifetime := time.Now().Add(life)
 	if lifetime.After(child.lifetime) {
 		child.duration = life
 		child.lifetime = lifetime
@@ -169,14 +185,15 @@ func (c *context) WithTimeout(life time.Duration) Context {
 
 	child.timer = time.AfterFunc(to, func() {
 		child.fields = nilPair
+		child.Cancel()
 	})
 
 	return child
 }
 
 // WithValue returns a new context based on the previos one.
-func (c *context) WithValue(key, value interface{}) Context {
-	child := c.newChild()
+func (c *context) WithValue(key, value interface{}) CancelableContext {
+	child := c.newChild(true)
 	child.fields = Append(child.fields, key, value)
 	return child
 }
@@ -187,9 +204,9 @@ func (c *context) TimeRemaining() (rem time.Duration, hasTimeout bool) {
 		return
 	}
 
-	now := time.Now()
-
 	hasTimeout = true
+
+	now := time.Now()
 	if now.Before(c.lifetime) {
 		rem = c.lifetime.Sub(now)
 		return
@@ -198,24 +215,14 @@ func (c *context) TimeRemaining() (rem time.Duration, hasTimeout bool) {
 	return
 }
 
-// WhenExpired returns a channel which gets closed when the context
+// Done returns a channel which gets closed when the context
 // has expired.
-func (c *context) WhenExpired() <-chan struct{} {
-	nc := make(chan struct{})
-
+func (c *context) Done() <-chan struct{} {
 	if c.IsExpired() {
-		close(nc)
-		return nc
+		return nil
 	}
 
-	rem, _ := c.TimeRemaining()
-
-	go func() {
-		defer close(nc)
-		<-time.After(rem)
-	}()
-
-	return nc
+	return c.canceller
 }
 
 // IsExpired returns true/false if the context internal data has expired.
@@ -227,19 +234,42 @@ func (c *context) IsExpired() bool {
 		}
 	}
 
+	c.cl.Lock()
+	{
+		if c.canceled {
+			c.cl.Unlock()
+			return true
+		}
+	}
+	c.cl.Unlock()
+
 	return false
+}
+
+// Ctx returns the Context interface  for a giving CancelableContext.
+func (c *context) Ctx() Context {
+	return c
 }
 
 // Cancel cancels the timer if there exists one set to clear context.
 func (c *context) Cancel() {
-	if c.timer != nil {
+	if c.IsExpired() {
 		return
 	}
 
-	c.timer.Stop()
+	c.cl.Lock()
+	c.canceled = true
+	c.cl.Unlock()
+
+	close(c.canceller)
+
+	if c.timer != nil {
+		c.timer.Stop()
+		return
+	}
 }
 
-// SetParent adds the giving value using the given key into the map into the
+// SetParent adds the giving value using the given key into the map of the
 // root parent of the context to have this persist to new context else sets
 // the value on itself if it has no parent.
 func (c *context) SetParent(key, val interface{}) {
@@ -263,11 +293,34 @@ func (c *context) Get(key interface{}) (item interface{}, found bool) {
 }
 
 // newChild returns a new fresh context based on the fields of this context.
-func (c *context) newChild() *context {
-	return &context{
-		parent:   c,
-		fields:   c.fields,
-		lifetime: c.lifetime,
-		duration: c.duration,
+func (c *context) newChild(cancelWithParent bool) *context {
+	canceller := make(chan struct{})
+
+	if c.IsExpired() {
+		close(canceller)
 	}
+
+	cm := &context{
+		parent:    c,
+		fields:    c.fields,
+		lifetime:  c.lifetime,
+		duration:  c.duration,
+		canceled:  c.canceled,
+		canceller: canceller,
+	}
+
+	if cancelWithParent {
+		go func() {
+			cancel := c.Done()
+			if cancel == nil {
+				cm.Cancel()
+				return
+			}
+
+			<-cancel
+			cm.Cancel()
+		}()
+	}
+
+	return cm
 }
---
+++ context/context_test.go
@@ -0,0 +1,94 @@
+package context_test
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/gu-io/gu/tests"
+	"github.com/influx6/faux/context"
+)
+
+// TestContextWithConnectedChild tests the validaty of the context.
+func TestContextWithConnectedChild(t *testing.T) {
+	ctx := context.New()
+	ctx.Set("brolly", "benzine")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go goRoutineContext(t, &wg, ctx.New(true))
+
+	go func() {
+		<-time.After(1 * time.Millisecond)
+		ctx.Cancel()
+	}()
+
+	wg.Wait()
+}
+
+// TestContextWithDisconnectedChild tests the validaty of the context.
+func TestContextWithDisconnectedChild(t *testing.T) {
+	ctx := context.New()
+	ctx.Set("brolly", "benzine")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go goRoutineContextFailure(t, &wg, ctx.WithTimeout(3*time.Millisecond, false))
+
+	go func() {
+		<-time.After(1 * time.Millisecond)
+		ctx.Cancel()
+	}()
+
+	wg.Wait()
+}
+
+// goRoutineContext tests the usage and canceling of the provided context variable.
+func goRoutineContext(t *testing.T, wg *sync.WaitGroup, ctx context.Context) {
+	defer wg.Done()
+
+	val, ok := ctx.Get("brolly")
+	if !ok {
+		tests.Failed(t, "Should have found key %q in the context", "brolly")
+	}
+	tests.Passed(t, "Should have found key %q in the context", "brolly")
+
+	realVal, _ := val.(string)
+	if realVal != "benzine" {
+		tests.Failed(t, "Should have matched context key %q with value %q", "brolly", "benzine")
+	}
+	tests.Passed(t, "Should have matched context key %q with value %q", "brolly", "benzine")
+
+	select {
+	case <-time.After(3 * time.Millisecond):
+		tests.Failed(t, "Should have successfully died through parent's call")
+	case <-ctx.Done():
+		tests.Passed(t, "Should have successfully died through parent's call")
+	}
+}
+
+// goRoutineContextFailure tests the usage and canceling of the provided context variable.
+func goRoutineContextFailure(t *testing.T, wg *sync.WaitGroup, ctx context.Context) {
+	defer wg.Done()
+
+	val, ok := ctx.Get("brolly")
+	if !ok {
+		tests.Failed(t, "Should have found key %q in the context", "brolly")
+	}
+	tests.Passed(t, "Should have found key %q in the context", "brolly")
+
+	realVal, _ := val.(string)
+	if realVal != "benzine" {
+		tests.Failed(t, "Should have matched context key %q with value %q", "brolly", "benzine")
+	}
+	tests.Passed(t, "Should have matched context key %q with value %q", "brolly", "benzine")
+
+	select {
+	case <-time.After(2 * time.Millisecond):
+		tests.Passed(t, "Should have successfully died through parent's call")
+	case <-ctx.Done():
+		tests.Failed(t, "Should have successfully died through parent's call")
+	}
+}
