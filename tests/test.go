a382cededdcb23704864f9711a7987031b7b01aa
Alexander Ewetumo <trinoxf@gmail.com>
2017-03-07 11:48:24 +0100

    changes: rewrite tests pkg

--- tests/test.go
+++ tests/test.go
@@ -2,6 +2,8 @@ package tests
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -9,8 +11,10 @@ import (
 const succeedMark = "\u2713"
 
 // Passed logs the failure message using the giving message and values.
-func Passed(t *testing.T, message string, val ...interface{}) {
-	t.Logf("\t%s\t %s", succeedMark, fmt.Sprintf(message, val...))
+func Passed(message string, val ...interface{}) {
+	if testing.Verbose() {
+		log.Output(2, fmt.Sprintf("\t%s\t %s\n", succeedMark, fmt.Sprintf(message, val...)))
+	}
 }
 
 // failedMark is the Unicode codepoint for an X mark.
@@ -18,10 +22,15 @@ const failedMark = "\u2717"
 
 // Failed logs the failure message using the giving message and values.
 func Failed(t *testing.T, message string, val ...interface{}) {
-	t.Fatalf("\t%s\t %s", failedMark, fmt.Sprintf(message, val...))
+	if testing.Verbose() {
+		log.Output(2, fmt.Sprintf("\t%s\t %s\n", failedMark, fmt.Sprintf(message, val...)))
+		os.Exit(1)
+	}
 }
 
 // Errored logs the error message using the giving message and values.
 func Errored(t *testing.T, message string, val ...interface{}) {
-	t.Errorf("\t%s\t %s", failedMark, fmt.Sprintf(message, val...))
+	if testing.Verbose() {
+		log.Output(2, fmt.Sprintf("\t%s\t %s\n", failedMark, fmt.Sprintf(message, val...)))
+	}
 }
