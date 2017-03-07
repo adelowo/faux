cbbb3c4537d6d70edb0adeb378cc6d7f927895c9
Alexander Ewetumo <trinoxf@gmail.com>
2017-03-07 12:04:35 +0100

    changes: rewrite tests pkg

--- tests/test.go
+++ tests/test.go
@@ -10,17 +10,19 @@ import (
 // succeedMark is the Unicode codepoint for a check mark.
 const succeedMark = "\u2713"
 
+var logger = log.New(os.Stdout, "", log.Lshortfile)
+
 // Info logs the info message using the giving message and values.
 func Info(message string, val ...interface{}) {
 	if testing.Verbose() {
-		log.Output(2, fmt.Sprintf("\t-\t %s\n", fmt.Sprintf(message, val...)))
+		logger.Output(2, fmt.Sprintf("\t-\t %s\n", fmt.Sprintf(message, val...)))
 	}
 }
 
 // Passed logs the failure message using the giving message and values.
 func Passed(message string, val ...interface{}) {
 	if testing.Verbose() {
-		log.Output(2, fmt.Sprintf("\t%s\t %s\n", succeedMark, fmt.Sprintf(message, val...)))
+		logger.Output(2, fmt.Sprintf("\t%s\t %s\n", succeedMark, fmt.Sprintf(message, val...)))
 	}
 }
 
@@ -30,7 +32,7 @@ const failedMark = "\u2717"
 // Failed logs the failure message using the giving message and values.
 func Failed(message string, val ...interface{}) {
 	if testing.Verbose() {
-		log.Output(2, fmt.Sprintf("\t%s\t %s\n", failedMark, fmt.Sprintf(message, val...)))
+		logger.Output(2, fmt.Sprintf("\t%s\t %s\n", failedMark, fmt.Sprintf(message, val...)))
 		os.Exit(1)
 	}
 }
@@ -38,6 +40,6 @@ func Failed(message string, val ...interface{}) {
 // Errored logs the error message using the giving message and values.
 func Errored(message string, val ...interface{}) {
 	if testing.Verbose() {
-		log.Output(2, fmt.Sprintf("\t%s\t %s\n", failedMark, fmt.Sprintf(message, val...)))
+		logger.Output(2, fmt.Sprintf("\t%s\t %s\n", failedMark, fmt.Sprintf(message, val...)))
 	}
 }
